fea0ba2a44d6fdb1417d2a72af8d8e33e2c7152a
Kenokoz <example@gmail.com>
2020-12-21 17:29:34 +0300

    'POST' requst for forms by formData

--- JS/script.js
+++ JS/script.js
@@ -211,5 +211,48 @@ window.addEventListener("DOMContentLoaded", () => {
         "menu__item"
     ).render();
 
+    // Forms
+
+    const forms = document.querySelectorAll("form");
+
+    const message = {
+        loading: "Загрузка",
+        success: "Спасибо! Скоро мы с вами свяжимся",
+        failure: "Что-то пошло не так..."
+    };
+    
+    forms.forEach(form => {
+        postData(form);
+    });
+    
+    function postData(form) {
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement("div");
+            statusMessage.classList.add("status");
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest(); // с данными formData заголовок НЕ НУЖЕН!
+            const formData = new FormData(form);  // в input ВСЕГДА должен указываться атрибут name
+
+            request.open("POST", "server.php");
+            request.send(formData);
+
+            request.addEventListener("load", () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
 });
 
